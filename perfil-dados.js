0ac44bff8a9a4a93a55e6c8e4a89159e5d6c52ff
LeilsonSSilva <leilson_silva@yahoo.com.br>
2023-03-03 21:18:51 -0300

    teclado

---
+++ perfil-dados.js
@@ -0,0 +1,9 @@
+const linkHome = document.getElementById("link-home")
+
+document.addEventListener("keyup", (e)=>{
+    console.log(e.code);
+
+    if(e.code == "Backspace"){
+        linkHome.click()
+    }
+})
---
+++ script.js
@@ -0,0 +1,23 @@
+const linkPerfil = document.getElementById("link-perfil");
+const navPerfil = document.getElementById("nav-perfil");
+const linkPerfilDados = document.getElementById("link-perfil-dados");
+
+linkPerfil.addEventListener("mouseover", ()=> {
+  navPerfil.style.display = "block"
+})
+
+window.addEventListener("keyup", (e)=>{
+  console.log(e.key)
+  console.log(e.code)
+
+  if(navPerfil.style.display == "block"){
+    if(e.code == "Digit1"){
+      linkPerfilDados.click()
+    }
+  } else if(e.code == "Digit1"){
+    console.log("Abrir navegação secundária")
+    navPerfil.style.display = "block"
+  }
+})
+
+
